bffc631e4215277255069a72e4e85a6d560b91e9
Kumar-Satyam-electropython <120978161+Kumar-Satyam-electropython@users.noreply.github.com>
2022-12-18 19:45:06 +0000

    Basic Format of the Package

--- electropython_tkinter_v0p0.py
+++ electropython_tkinter_v0p0.py
@@ -1,1 +1,9 @@
 print("Welcome to electropython_tkinter_v0p0")
+
+# Important: This package is open to re-use copy and distribute, but you need to mention the \
+# sourse path as my repository link on github. 
+# 
+# Description: This code is a package which will be used to create an electronics simulator, \
+# small games, hardware controlling code etc..
+
+
